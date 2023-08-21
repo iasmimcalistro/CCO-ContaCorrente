749bd72e90bba69c62a7dcd3df15a03b5ca425b4
iasmimcalistro <iascalistro@gmail.com>
2023-08-20 22:00:39 -0300

    Adicionar arquivos de projeto.

---
+++ CCO-ContaCorrente/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+
+namespace CCO_ContaCorrente
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+
+        public string Titular { get; set; }
+
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular){
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this (numero, titular) {
+            Saldo = saldo;
+
+        }
+
+        public override string ToString()
+        {
+            return "Conta"
+                + Numero
+                + "Titular"
+                + Titular
+                + "Saldo"
+                + Saldo.ToString("F3", CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
---
+++ CCO-ContaCorrente/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("CCO-ContaCorrente")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CCO-ContaCorrente")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("47ab50e6-19fb-41a5-8409-ae654099f7c9")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
